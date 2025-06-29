a6430125999814471adcf67ca9ccf4f33c7ee862
kacode357 <kacode357@gmail.com>
2025-06-29 13:42:26 +0700

    1.0.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'MediConnect',
+  description: 'MediConnect là nền tảng mạng xã hội y khoa hàng đầu Việt Nam, nơi kết nối bác sĩ, chuyên gia y tế, và cộng đồng. Chia sẻ kiến thức, kinh nghiệm, và tìm kiếm thông tin y tế đáng tin cậy.',
+  generator: 'kalus',
 }
 
 export default function RootLayout({
